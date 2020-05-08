55d4b6af1e05bc965c347cc70bdbfc9955bba2c4
clxye <chenlia2602@sina.com>
2020-05-08 08:53:37 +0800

    second commit

--- src/main/java/com/example/springideagithubjenkins/SpringIdeaGithubJenkinsApplication.java
+++ src/main/java/com/example/springideagithubjenkins/SpringIdeaGithubJenkinsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringIdeaGithubJenkinsApplication {
+public class SpringIdeaGithubJenkinsApplication
 
     public static void main(String[] args) {
         SpringApplication.run(SpringIdeaGithubJenkinsApplication.class, args);
